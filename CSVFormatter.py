5d2b9e5fcfe8be4510ec5fc8e380c82002265de2
Joshua Zosky <joshua.e.zosky@gmail.com>
2016-10-14 16:52:30 -0500

    Automated file-loading via Input/Output folders

--- CSVFormatter.py
+++ CSVFormatter.py
@@ -1,60 +1,106 @@
 """
-Get Metadata from CSV file
+    Copyright 2016 Joshua Zosky
+    joshua.e.zosky@gmail.com
 
-Metadata order:
-Column 1 = File title
-Column 2 = Date of recording
-Column 3 = Sampling Rate
-Column 4 = Subject #
-Column 5 = Data Labels
-Column 6 = Number of Columns in Dataset
-Column 7 = 'units:emotiv' (unsure what this is for)
+    This file is part of "Emotiv Epoch Tools".
+    "Emotiv Epoch Tools" is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    "Emotiv Epoch Tools" is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with "Emotiv Epoch Tools".  If not, see <http://www.gnu.org/licenses/>.
 
+    Reorganize Emotiv EEG files using their Metadata header.
+
+    Step 1)
+    Get Metadata from CSV file.
+
+    Metadata order:
+    Column 1 = File title
+    Column 2 = Date of recording
+    Column 3 = Sampling Rate
+    Column 4 = Subject #
+    Column 5 = Data Labels
+    Column 6 = Number of Columns in Dataset
+    Column 7 = 'units:emotiv' (unsure what this is for)
+
+    Step 2)
+    Save Metadata to separate file.
+
+    Step 3)
+    Replace CSV header with the Data Labels.
+
+    Step 4)
+    Save files to separate folders with new names based on
+        Subject ID # and date/time of recording.
 """
 
 import csv
+import os
+import sys
 
-single_file1 = 'SampleCSV.csv'
-single_file2 = 'SampleCSV2.csv'
+list_of_csvs = []
+counter = 0
+print "Opening Input Folder\n==================="
+for root, subdirs, files in os.walk('Input'):
+    for filename in files:
+        if filename.lower().endswith('.csv'):
+            list_of_csvs.append(filename)
+            counter += 1
+            sys.stdout.write("There are %d files \r" % counter)
+            sys.stdout.flush()
+    print '\n'
+    print list_of_csvs
 
-with open(single_file1) as open_file:
-    csv_object = csv.reader(open_file, delimiter=',')
-    csv_metadata = csv_object.next()
+print "\nConverting Files\n==================="
+completed_counter = 0
+for single_file in list_of_csvs:
+    sys.stdout.write("\r%s Files completed: %d/%d    " % (single_file, completed_counter, counter))
+    sys.stdout.flush()
+    with open('Input/%s' % single_file) as open_file:
+        csv_object = csv.reader(open_file, delimiter=',')
+        csv_metadata = csv_object.next()
 
-csv_metadata = [item.strip(' ') for item in csv_metadata]
-csv_dictionary = {}
-for item in csv_metadata:
-    if ':' in item:
-        item_split = item.split(':')
-        item_key = item_split[0]
-        item_value = item_split[1]
-        csv_dictionary[item_key] = item_value
+    csv_metadata = [item.strip(' ') for item in csv_metadata]
+    csv_dictionary = {}
+    for item in csv_metadata:
+        if ':' in item:
+            item_split = item.split(':')
+            item_key = item_split[0]
+            item_value = item_split[1]
+            csv_dictionary[item_key] = item_value
 
-field_names = [
-    'title',
-    'recorded',
-    'sampling',
-    'subject',
-    'labels',
-    'chan',
-    'units'
-]
+    field_names = [
+        'title',
+        'recorded',
+        'sampling',
+        'subject',
+        'labels',
+        'chan',
+        'units'
+    ]
 
-header_filename = 'Subject %s %s - header.csv' % (csv_dictionary['subject'], csv_dictionary['recorded'])
+    header_filename = 'Subject %s %s.csv' % (csv_dictionary['subject'], csv_dictionary['recorded'])
 
-with open(header_filename, 'w') as out_file:
-    writer = csv.DictWriter(out_file, fieldnames=field_names)
-    writer.writeheader()
-    writer.writerow(csv_dictionary)
+    with open('Output/Info/%s' % header_filename, 'w+') as out_file:
+        writer = csv.DictWriter(out_file, fieldnames=field_names)
+        writer.writeheader()
+        writer.writerow(csv_dictionary)
 
-new_header = csv_dictionary['labels'].split()
+    new_header = csv_dictionary['labels'].split()
 
-with open(single_file1) as in_file:
-    #in_file.next()
-    data = csv.reader(in_file)
-    data.next()
+    with open('Input/%s' % single_file) as in_file:
+        data = csv.reader(in_file)
+        data.next()
 
-    with open(single_file2, 'w') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(new_header)
-        writer.writerows(data)
+        with open('Output/Data/%s' % header_filename, 'w+') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(new_header)
+            writer.writerows(data)
+    completed_counter += 1
+
+sys.stdout.write("\rCompleted converting %d files                                         \n" % counter)
